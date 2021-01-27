52868582c948239300baba53ecf8ec30c344bb56
Cameron Nix <camerondeverenix@gmail.com>
2021-01-27 03:48:14 -0500

    updated sturcture and pseudocoding for burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,24 +1,22 @@
 const orm = require("../config/orm.js");
 
 const burgers = {
-// Display all burgers in bucket list.
-select: (callBack) => {
-    orm.select("burgers", (res) => callBack(res));
-},
-// Add new burger to bucket list.
-insert: (newBurger, callBack) => {
-    orm.insert("burgers", "burger_name", "devoured", newBurger, (res) => callBack(res));
-},
-// Move burger from bucket list to devoured lit.
-update: (burgerDevoured, callBack) => {
-    orm.update("burgers", "devoured", "id", burgerDevoured, (res) => callBack(res));
-},
-// Remove bruger from devoured list.
-delete: (burgerGone, callBack) => {
-    orm.delete("burgers", "id", burgerGone, (res) => callBack(res));
-},
-
+    // Display all burgers in bucket list.
+    select: (callBack) => {
+        orm.select("burgers", (res) => callBack(res));
+    },
+    // Add new burger to bucket list.
+    insert: (newBurger, callBack) => {
+        orm.insert("burgers", "burger_name", "devoured", newBurger, (res) => callBack(res));
+    },
+    // Move burger from bucket list to devoured lit.
+    update: (burgerDevoured, callBack) => {
+        orm.update("burgers", "devoured", "id", burgerDevoured, (res) => callBack(res));
+    },
+    // Remove bruger from devoured list.
+    delete: (burgerGone, callBack) => {
+        orm.delete("burgers", "id", burgerGone, (res) => callBack(res));
+    }
 };
-
 
 module.exports = burgers;
